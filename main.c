0fe39f118b81bb8b85d7d385de99e4754039bc96
nusrat78 <njahan5061@gmail.com>
2022-12-09 16:16:09 +0600

    new

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 char box[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-char box2[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+const char box2[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 void createBoard();
 int checkWinner();
 int main()
